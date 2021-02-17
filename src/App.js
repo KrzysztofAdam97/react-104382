fa7fb550bbfa8dbb187e971074ff98cbcab4590a
Krzysztof Guzenda <k.guzenda1@gmail.com>
2021-02-17 23:32:30 +0100

    KG

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {Container, Row, Col} from "react-bootstrap";
+
+const App = () => {
+  return (
+  <div className="app">
+    <Container className="gr5__container">
+      <Row>
+        <Col>
+          <h1>Baner</h1>
+        </Col>
+        <Col>
+          <h1>Baner</h1>
+        </Col>
+      </Row>
+    </Container>
+
+  <h1>Wyszukiwarka</h1>
+  <h1>Wyniki wyszukiwania</h1>
+   
+    </div>
+    );
+};
+
+export default App;
