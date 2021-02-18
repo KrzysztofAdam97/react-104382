8226613a28824ab76a92e492f190476afc01daba
KrzysztofAdam97 <74449932+KrzysztofAdam97@users.noreply.github.com>
2021-02-19 00:29:18 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Container, Row, Col, Jumbotron, Button} from "react-bootstrap";
-
+import Header from "./components/header/Header";
+import Search from "./components/search/Search";
+import Search2 from "./components/search2/Search2";
 const App = () => {
   return (
   <div className="app">
     <Container className="gr5__container">
-      <Jumbotron className="gr5__container">
-  <h1>Hello, world!</h1>
+      <Header />
+      <p2>Podaj kwotę</p2>
+      <Search />
+      <p3>Przelicz na:</p3>
+      <Search2 />
+      <p5>Wynik:</p5>
+      {/* <p4>Przelicz na:</p4> */}
+
+ 
+
+      {/* <Jumbotron className="jumbo">
+  <h1>Kantor - wymiana walut</h1>
   <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
+    U nas najtaniej na rynku wymienisz swoją obca walute. 0 prowizji. 0 odsetek. Totalne 0 kosztów.
+  </p>
+  <p>
+    <Button variant="primary">Sprawdź aktualne kursy walut</Button>
   </p>
   <p>
     <Button variant="primary">Learn more</Button>
   </p>
-      </Jumbotron>
+      </Jumbotron> */}
       {/* <Row>
         <Col className="gr5__col1">
           <h1>Baner</h1>
