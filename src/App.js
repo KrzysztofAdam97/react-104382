6398c674d34b672d599d1056974d8960786023fa
KrzysztofAdam97 <74449932+KrzysztofAdam97@users.noreply.github.com>
2021-02-18 22:15:56 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Jumbotron, Button} from "react-bootstrap";
 
 const App = () => {
   return (
   <div className="app">
     <Container className="gr5__container">
-      <Row>
-        <Col>
+      <Jumbotron className="gr5__container">
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Learn more</Button>
+  </p>
+      </Jumbotron>
+      {/* <Row>
+        <Col className="gr5__col1">
           <h1>Baner</h1>
         </Col>
-        <Col>
+        <Col className="gr5__col2">
           <h1>Baner</h1>
         </Col>
-      </Row>
+      </Row> */}
     </Container>
 
-  <h1>Wyszukiwarka</h1>
-  <h1>Wyniki wyszukiwania</h1>
+  {/* <h1>Wyszukiwarka</h1>
+  <h1>Wyniki wyszukiwania</h1> */}
    
     </div>
     );
